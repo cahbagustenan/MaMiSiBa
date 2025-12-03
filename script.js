88de71c1cb81e6fd16704933fb1ff8a085a7b32b
YohanesOktavianto <fiantookta314@gmail.com>
2025-12-03 19:02:41 +0700

    Initial commit for Vercel website

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: tombol menu & pesan
+document.querySelector(".btn-menu").addEventListener("click", function () {
+  alert("Menu makanan akan ditampilkan di halaman baru nanti.");
+});
+
+document.querySelector(".btn-pesan").addEventListener("click", function () {
+  alert("Silakan hubungi kami melalui WhatsApp!");
+});
